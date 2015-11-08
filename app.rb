719b791339d7bf4f5cd56b361f94f6dd73222c6a
pcm020 <pacab@hotmail.com>
2015-11-08 03:32:23 +0000

    project commit

--- app.rb
+++ app.rb
@@ -40,6 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      r = @game.guess(letter)
+      flash[:message] =  "You have already used that letter." if r == false
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,17 +56,32 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    when :play
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,16 +2,52 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
+  ROUNDS = 7
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  def initialize()
+    @word = get_random_word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
-
+  
+  def guess(letter)
+    raise(ArgumentError) if letter == nil || letter == '' || letter.match(/[^a-zA-Z]/)
+    l = letter[0].downcase
+    return false if guesses.count(l)>0 || wrong_guesses.count(l)>0
+    if word.count(l)>0
+      @guesses+=l
+    else
+      @wrong_guesses+=l
+    end
+  end
+  
+  def word_with_guesses
+    result = ''
+    @word.each_char do |c| 
+      if @guesses.count(c) > 0 
+        result += c
+      else
+        result += '-'
+      end
+    end
+        
+    return result
+  end
+  
+  def check_win_or_lose
+    return :win if word_with_guesses.count('-') == 0
+    return :play if @wrong_guesses.length < ROUNDS
+    return :lose
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -19,4 +55,15 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 end
+
+#game=HangpersonGame.new('banana')
+#print game.word
+#print game.guesses
+#game.guess('b')
+#puts game.guesses
+#puts game.word_with_guesses
+#puts game.check_win_or_lose
